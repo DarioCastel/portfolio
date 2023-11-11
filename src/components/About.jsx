cfbd32d508a8f06b5a0d058dc24d663ad4dd2961
DarioCastel <lecastelnovo@gmail.com>
2023-11-11 10:29:28 -0300

    moreIcons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,22 +1,31 @@
-import "../css/about.scss"
+import "../css/about.scss";
+import { SiJavascript, SiHtml5, SiCss3,SiMongodb } from "react-icons/si";
+import { FaReact } from "react-icons/fa6";
+
 
 const About = () => {
   return (
     <>
-    <div className="aboutContainer">
+      <div className="aboutContainer">
         <h1 className="h1About">Sobre mi</h1>
-        <p className="pAbout">hola soy la wea fome
-        Estoy emocionado por la oportunidad de conectarme contigo. Explora mi portafolio para obtener una visión más profunda de mis proyectos y habilidades. ¡Si tienes una idea emocionante o proyecto en mente, hablemos!
-        
-        Estas son las tecnologias con las que trabajo</p>
+        <p className="pAbout">
+          hola soy la wea fome Estoy emocionado por la oportunidad de conectarme
+          contigo. Explora mi portafolio para obtener una visión más profunda de
+          mis proyectos y habilidades. ¡Si tienes una idea emocionante o
+          proyecto en mente, hablemos! <br />
+          <br />
+          Estas son las tecnologias con las que trabajo:
+        </p>
         <div className="buttonL">
-            boton1
-            boton2
-            boton3
+          <SiJavascript className="tecnoIcon"/>
+          <SiHtml5 className="tecnoIcon"/>
+          <SiCss3 className="tecnoIcon"/>
+          <FaReact className="tecnoIcon"/>
+          <SiMongodb className="tecnoIcon"/>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default About
+export default About;
