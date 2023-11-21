6e3cb9a3fe8eede1356217354f6545f3b70402f0
DarioCastel <lecastelnovo@gmail.com>
2023-11-21 20:34:23 -0300

    contact v2

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -1,39 +1,59 @@
 import "../css/formContact.scss";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 const FormContact = () => {
   const form = useRef();
+  const [valid, setValid] = useState(true)
 
-  const validate = (e) => {
-    const username1 = e.target.user_name.value;
-    const email = e.target.user_Email.value;
+  const validate = () => {
+    const user_name = form.current.user_name.value;
+    const user_Email = form.current.user_Email.value;
+    const message = form.current.message.value;
 
-    if (username1 == "" || email == "") {
-      console.log("completar campos")
+    const mailregex =
+      /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+
+    if (user_name === "") {
+    setValid(false)
+      console.log("completar userName")
+      return;
+    }
+  
+    if (user_name === "" || !mailregex.test(user_Email)) {
+    setValid(false)
+      console.log("completar userName")
+      return;
+    }
+  
+    if (message === "") {
+    setValid(false)
+      console.log("completar el mensaje")
+      return;
     }
   };
 
   const sendEmail = (e) => {
     e.preventDefault();
     validate();
-
-    emailjs
-      .sendForm(
-        "service_bl2idx9",
-        "template_rg1e85m",
-        form.current,
-        "aghokcv3l9xzt0U9m"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    if (valid) {
+      emailjs
+        .sendForm(
+          "service_bl2idx9",
+          "template_rg1e85m",
+          form.current,
+          "aghokcv3l9xzt0U9m"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    }
   };
 
   return (
