13a4b550d609b9f66a5ec7a98fc3490550988b85
DarioCastel <lecastelnovo@gmail.com>
2023-11-30 20:39:56 -0300

    contactFixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,55 +2,56 @@ import { useState } from "react";
 import "../css/contact.scss";
 import Alert from "./Alert";
 import FormContact from "./FormContact";
-
+import { FaWhatsapp } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
 
 const Contact = () => {
-  const [error, setError] = useState(
-    {
-      textError:"",
-      typeError:"",
-      active:""
-    }
-  )
+  const [error, setError] = useState({
+    textError: "",
+    typeError: "",
+    active: "",
+  });
   return (
     <>
-      <div className="contact-form-container">
-        <Alert 
+      <Alert
         textError={error.textError}
         typeError={error.typeError}
         active={error.active}
-        />
-        <div className="contact-us">
-          <div className="contact-header">
-            <h1>&#9135;&#9135;&#9135;&#9135;&nbsp;&nbsp;CONTACT US</h1>
-          </div>
-          <div className="social-bar">
-            <ul>
-              <li>
-                <i className="fab fa-facebook-f"></i>
-              </li>
-              <li>
-                <i className="fab fa-twitter"></i>
-              </li>
-              <li>
-                <i className="fab fa-instagram"></i>
-              </li>
-              <li>
-                <i className="fab fa-dribbble"></i>
-              </li>
-            </ul>
-          </div>
+      />
+      <div className="contactCont">
+        <h2 className="h2Contact">contact me</h2>
+        <div className="contact-form">
+          <FormContact setError={setError} />
         </div>
         <div className="infoPersonal">
-        <a href="https://wa.me/5493764211005" className="wasap"  target="_blank" rel="noreferrer">
-          wasapeame
-        </a>
-        <a href="lecastelnovo@gmail.com" className="mailcontact" target="_blank" rel="noreferrer">mail me</a>
-        </div>
-        <div className="contact-form">
-          <FormContact
-          setError={setError}
-          />
+          <a
+            href="https://wa.me/5493764211005"
+            className="wasap"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="whatsappCont">
+              <span className="whatsappText">
+              whatsapp me<FaWhatsapp /><br/>
+              </span>
+              +543764211005
+            </div>
+            
+
+          </a>
+          <a
+            href="lecastelnovo@gmail.com"
+            className="mailContact"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="emailCont">
+              <span className="emailText">
+              mail me<HiOutlineMail /><br/>
+              </span>
+              lecastelnovo@gmail.com
+            </div>
+          </a>
         </div>
       </div>
     </>
